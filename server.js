602168531b602432d68b46a91c462a4e8c78d145
Dexter <rille.dexter@marsu.edu.ph>
2026-03-10 12:55:14 +0800

    dipatapos

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ const BASE_URI = process.env.BASE_URI || '/api/v1/';
 
 // Import Routes 
 const apiRoutes = require('./src/routes/apiRoutes');
+const authRoutes = require('./src/routes/authRoutes')
 app.use(process.env.BASE_URI, apiRoutes);
+app.use(process.env.BASE_URI, authRoutes);
 
 app.listen(PORT, () => {
     console.log (`Server running on port ${PORT}`);
